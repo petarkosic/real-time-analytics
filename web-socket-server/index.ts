648676859f456b150078aab734f0076c352305b5
petarkosic <avt3914@gmail.com>
2025-01-24 23:55:53 +0100

    fix: web socket server
    Setup server to query the database and aggregate data into time buckets.

--- web-socket-server/index.ts
+++ web-socket-server/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import http from 'http';
 import cors from 'cors';
 import { Server } from 'socket.io';
-import amqp from 'amqplib';
+import { Pool } from 'pg';
 
 const app = express();
 
@@ -16,47 +16,101 @@ const io = new Server(server, {
 	},
 });
 
-const rabbitMQConnOptions = {
-	protocol: process.env.RABBITMQ_PROTOCOL,
-	hostname: process.env.RABBITMQ_HOSTNAME,
-	port: Number(process.env.RABBITMQ_PORT),
-	username: process.env.RABBITMQ_USER,
-	password: process.env.RABBITMQ_PASS,
-};
+const pool = new Pool({
+	user: process.env.POSTGRES_USER,
+	host: process.env.POSTGRES_HOST,
+	database: process.env.POSTGRES_DB,
+	password: process.env.POSTGRES_PASSWORD,
+	port: Number(process.env.POSTGRES_PORT),
+});
 
-async function setupRabbitMQConsumer() {
-	try {
-		const connection = await amqp.connect(rabbitMQConnOptions);
-		const channel = await connection.createChannel();
-		await channel.assertQueue('stock_price_queue', { durable: true });
+io.on('connection', (socket) => {
+	console.log('A user connected');
 
-		io.on('connection', (socket) => {
-			console.log('A user connected');
+	socket.on('stock', async (data) => {
+		const { interval, symbol } = data;
 
-			socket.on('subscribe', async (symbols) => {
-				channel.consume('stock_price_queue', async (msg) => {
-					if (msg) {
-						const stockData = JSON.parse(msg.content.toString());
+		const bucketSize = getBucketSize(interval);
+		if (!bucketSize) {
+			console.error('Invalid interval');
+			return;
+		}
 
-						if (symbols.includes(stockData.symbol)) {
-							socket.emit('stock', [stockData]);
-						}
+		const client = await pool.connect();
 
-						channel.ack(msg);
-					}
-				});
-			});
+		try {
+			client.query('BEGIN');
 
-			socket.on('disconnect', () => {
-				console.log('A user disconnected');
-			});
-		});
-	} catch (error) {
-		console.error('Error connecting to RabbitMQ', error);
+			const query = `
+	        SELECT symbol, time_bucket_gapfill($1, timestamp) AS bucket,
+	               locf(AVG(price)) AS avg_price,
+	               locf(MAX(price)) AS max_price,
+	               locf(MIN(price)) AS min_price
+	        FROM raw_stock_data
+	        WHERE symbol = $2 AND
+			timestamp >= NOW() - INTERVAL '${getMaxInterval(interval)}' AND
+			timestamp < NOW()
+	        GROUP BY bucket, symbol
+	        ORDER BY bucket;
+	      `;
+
+			const result = await client.query(query, [bucketSize, symbol]);
+
+			const aggregatedData = result.rows.map((row) => ({
+				symbol,
+				bucket: row.bucket,
+				price: row.avg_price,
+				avgPrice: row.avg_price,
+				maxPrice: row.max_price,
+				minPrice: row.min_price,
+			}));
+
+			socket.emit('stock_data', { [symbol]: aggregatedData });
+
+			client.query('COMMIT');
+		} catch (error) {
+			client.query('ROLLBACK');
+			console.error('Error executing query', error);
+		} finally {
+			client.release();
+		}
+	});
+
+	socket.on('disconnect', () => {
+		console.log('A user disconnected');
+	});
+});
+
+function getBucketSize(interval: string): string | null {
+	switch (interval) {
+		case '1m':
+			return '1 minute';
+		case '5m':
+			return '5 minutes';
+		case '30m':
+			return '30 minutes';
+		case '1h':
+			return '1 hour';
+		default:
+			return null;
 	}
 }
 
-server.listen(5000, async () => {
+function getMaxInterval(interval: string): string {
+	switch (interval) {
+		case '1m':
+			return '2 hours';
+		case '5m':
+			return '6 hours';
+		case '30m':
+			return '24 hours';
+		case '1h':
+			return '48 hours';
+		default:
+			return '24 hours';
+	}
+}
+
+server.listen(5000, () => {
 	console.log('Server is running on port 5000');
-	await setupRabbitMQConsumer();
 });
